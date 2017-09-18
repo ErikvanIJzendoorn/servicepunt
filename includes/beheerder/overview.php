c2883833926e4d29207ace00142a0e346328867b
YvetteVoskuijlen <yvette.voskuijlen@gmail.com>
2017-09-18 12:16:15 +0200

    beheeerder

---
+++ includes/beheerder/overview.php
@@ -0,0 +1,57 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Servicepunt:Overzicht</title>
+		<link rel="stylesheet" href="../../assets/css/main.css">
+		<link rel="stylesheet" href="../../assets/libs/materialize/css/materialize.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script rel="materialize" src="../../assets/libs/materialize/js/materialize.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<script rel="register_modal" src="../../assets/js/register_info.js"></script>
+ 		<style>
+			body{
+				background-color: #424242;
+			}
+		</style>
+	</head>
+
+<body>
+	<div class="section"></div>
+	<main>
+		<ul id="nav-mobile" class="side-nav fixed orange accent-4 white-text" style="overflow: auto; transform: translateX(0%);">
+	        <li class="bold"><a href="" class="waves-effect waves-light white-text">Over</a></li>
+	        <li class="bold"><a href="" class="waves-effect waves-light white-text">Aan de slag</a></li>
+	        <li class="bold"><a href="" class="waves-effect waves-light white-text">Aan de slag</a></li>
+    	</ul>
+	</main>	
+
+	<div class="container" style="margin-top: 100px;">
+		<h5 class="orange-text text-accent-4 unselectable">Inloggen</h5>
+			<div class='row'>
+				<div class='col s12'>
+				</div>
+			</div>
+
+			<div class='row'>
+				<div class='input-field col s12'>
+					<input class='validate' type='text' name='username' id='username' style="margin-bottom: 0;" />
+					<label class='unselectable' for='email'>Gebruikersnaam</label>
+				</div>
+			</div>
+
+			<div class='row'>
+				<div class='input-field col s12'>
+					<input class='validate' type='password' name='password' id='password' />
+					<label class='unselectable' for='password'>Wachtwoord</label>
+				</div>
+			</div>
+
+			<center>
+				<div class='row'>
+					<button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect orange accent-4 unselectable'>Login</button>
+				</div>
+		</div>
+	</div>	
+</body>
+</html>
