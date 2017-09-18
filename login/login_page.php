6d3af627f781853b92290b6d72950efe9cf2ac7b
ErikvanIJzendoorn <erik.vanijzendoorn@hotmail.com>
2017-09-18 10:26:20 +0200

    Login Page Fix

--- login/login_page.php
+++ login/login_page.php
@@ -2,17 +2,14 @@
 <head>
 	<meta charset="utf-8">
 	<title>Login</title>
-	<link rel="stylesheet" href="../assets/css/main.css">
-	<link rel="stylesheet" href="../assets/css/login.css">
-	<link rel="stylesheet" href="../assets/libs/materialize/css/materialize.min.css">
-	<script rel="materialize" src="../assets/libs/materialize/js/materialize.min.js"></script>
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="../assets/css/main.css">
-
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-	<link rel="stylesheet" href="../assets/css/login.css">
+	<link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="../assets/libs/materialize/css/materialize.min.css">
+    <script rel="materialize" src="../assets/libs/materialize/js/materialize.min.js"></script>
 </head>
 
 <body>
@@ -22,11 +19,11 @@
 	        <div class="z-depth-1 grey lighten-4 row formWrap">
                 <form class="col s12 formDiv" method="post">
 
-                <h5 class="orange-text text-accent-4 unselectable">Inloggen</h5>
+                <h5 class="col s12 orange-text text-accent-4 unselectable">Inloggen</h5>
 
                 <div class='input-field col s12'>
                     <input class='validate' type='text' name='username' id='username'/>
-                    <label class='unselectable' for='email'>Gebruikersnaam</label>
+                    <label class='unselectable' for='username'>Gebruikersnaam</label>
                 </div>
 
                 <div class='input-field col s12'>
