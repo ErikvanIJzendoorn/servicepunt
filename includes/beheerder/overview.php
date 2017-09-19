708f378e205c91c4677b2bf5f386c6dc7b2228c0
YvetteVoskuijlen <yvette.voskuijlen@gmail.com>
2017-09-19 09:09:07 +0200

    tab fix

--- includes/beheerder/overview.php
+++ includes/beheerder/overview.php
@@ -18,40 +18,14 @@
 
 <body>
 	<div class="section"></div>
-	<main>
-		<ul id="nav-mobile" class="side-nav fixed orange accent-4 white-text" style="overflow: auto; transform: translateX(0%);">
-	        <li class="bold"><a href="" class="waves-effect waves-light white-text">Over</a></li>
-	        <li class="bold"><a href="" class="waves-effect waves-light white-text">Aan de slag</a></li>
-	        <li class="bold"><a href="" class="waves-effect waves-light white-text">Aan de slag</a></li>
+	<header>
+		<ul id="nav-mobile" class="side-nav fixed orange accent-4 white-text" style="width: 12.5%">
+	        <li class="bold"><a href="http://localhost/SCHOOL/servicepunt/includes/beheerder/overview.php" class="waves-effect waves-light white-text active">Overzicht</a></li>
+	        <li class="bold"><a href="http://localhost/SCHOOL/servicepunt/includes/beheerder/toevoegen.php" class="waves-effect waves-light white-text">Producten toevoegen</a></li>
+	        <li class="bold"><a href="http://localhost/SCHOOL/servicepunt/includes/beheerder/verwijderen.php" class="waves-effect waves-light white-text">Producten verwijderen</a></li>
     	</ul>
-	</main>	
-
-	<div class="container" style="margin-top: 100px;">
-		<h5 class="orange-text text-accent-4 unselectable">Inloggen</h5>
-			<div class='row'>
-				<div class='col s12'>
-				</div>
-			</div>
-
-			<div class='row'>
-				<div class='input-field col s12'>
-					<input class='validate' type='text' name='username' id='username' style="margin-bottom: 0;" />
-					<label class='unselectable' for='email'>Gebruikersnaam</label>
-				</div>
-			</div>
-
-			<div class='row'>
-				<div class='input-field col s12'>
-					<input class='validate' type='password' name='password' id='password' />
-					<label class='unselectable' for='password'>Wachtwoord</label>
-				</div>
-			</div>
-
-			<center>
-				<div class='row'>
-					<button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect orange accent-4 unselectable'>Login</button>
-				</div>
-		</div>
-	</div>	
+	</header>	
+	<main>
+	</main>
 </body>
 </html>
